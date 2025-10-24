7ee8e2d7684ef5cc7d6e5659924f1dd763b8b74d
harsamerveersingh <harsamerveersinghcostco@gmail.com>
2025-10-24 14:09:58 -0700

    Q1_initial_commit

---
+++ Analysis Code/Q1.R
@@ -0,0 +1,4 @@
+# Research Question1: 
+# Metrics: Population of each area, closeness centrality of each area in the network, find underserved score = population/closeness centrality, highlighting areas with high population but low accessibility.
+# Analysis: Identify areas with high underserved scores as priority zones. These are the areas where adding backup stations would likely have the most impact. Plot a graph to visualize results.
+
