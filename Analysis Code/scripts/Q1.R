0a523b723e0cb7618ada71957ccf23ef84677b64
hsingh77@student.ubc.ca <hsingh77@student.ubc.ca>
2025-11-03 13:17:22 -0800

    # Research Question1:  Setup

--- Analysis Code/scripts/Q1.R
+++ Analysis Code/scripts/Q1.R
@@ -1,4 +1,5 @@
 # Research Question1: 
-# Metrics: Population of each area, closeness centrality of each area in the network, find underserved score = population/closeness centrality, highlighting areas with high population but low accessibility.
-# Analysis: Identify areas with high underserved scores as priority zones. These are the areas where adding backup stations would likely have the most impact. Plot a graph to visualize results.
 
+# Metrics: Population of each area, closeness centrality of each area in the network, find underserved score = population/closeness centrality, highlighting areas with high population but low accessibility.
+
+# Analysis: Identify areas with high underserved scores as priority zones. These are the areas where adding backup stations would likely have the most impact. Plot a graph to visualize results.
